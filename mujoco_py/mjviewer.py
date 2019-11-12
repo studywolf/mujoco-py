470dcd3287bbac7f0fdd9ab67c819c75a95ba4d4
travis dewolf <travis.dewolf@gmail.com>
2019-11-12 11:10:15 -0500

    Cleaned up viewer for demo

--- mujoco_py/mjviewer.py
+++ mujoco_py/mjviewer.py
@@ -293,46 +293,15 @@ class MjViewer(MjViewerBasic):
             else:
                 self.add_overlay(const.GRID_TOPLEFT, "Run speed = %.3f x real time" %
                                 self._run_speed, "[S]lower, [F]aster")
-            self.add_overlay(
-                const.GRID_TOPLEFT, "Ren[d]er every frame", "Off" if self._render_every_frame else "On")
             self.add_overlay(const.GRID_TOPLEFT, "Switch camera (#cams = %d)" % (self._ncam + 1),
                                                 "[Tab] (camera ID = %d)" % self.cam.fixedcamid)
-            self.add_overlay(const.GRID_TOPLEFT, "[C]ontact forces", "Off" if self.vopt.flags[
-                            10] == 1 else "On")
-            self.add_overlay(
-                const.GRID_TOPLEFT, "Referenc[e] frames", "Off" if self.vopt.frame == 1 else "On")
-            self.add_overlay(const.GRID_TOPLEFT,
-                            "T[r]ansparent", "On" if self._transparent else "Off")
-            self.add_overlay(
-                const.GRID_TOPLEFT, "Display [M]ocap bodies", "On" if self._show_mocap else "Off")
             if self._paused is not None:
                 if not self._paused:
                     self.add_overlay(const.GRID_TOPLEFT, "Stop", "[Space]")
                 else:
                     self.add_overlay(const.GRID_TOPLEFT, "Start", "[Space]")
-                self.add_overlay(const.GRID_TOPLEFT,
-                                "Advance simulation by one step", "[right arrow]")
             self.add_overlay(const.GRID_TOPLEFT, "[H]ide Menu", "")
-            if self._record_video:
-                ndots = int(7 * (time.time() % 1))
-                dots = ("." * ndots) + (" " * (6 - ndots))
-                self.add_overlay(const.GRID_TOPLEFT,
-                                "Record [V]ideo (On) " + dots, "")
-            else:
-                self.add_overlay(const.GRID_TOPLEFT, "Record [V]ideo (Off) ", "")
-            if self._video_idx > 0:
-                fname = self._video_path % (self._video_idx - 1)
-                self.add_overlay(const.GRID_TOPLEFT, "   saved as %s" % fname, "")
 
-            self.add_overlay(const.GRID_TOPLEFT, "Cap[t]ure frame", "")
-            if self._image_idx > 0:
-                fname = self._image_path % (self._image_idx - 1)
-                self.add_overlay(const.GRID_TOPLEFT, "   saved as %s" % fname, "")
-            self.add_overlay(const.GRID_TOPLEFT, "Start [i]pdb", "")
-            if self._record_video:
-                extra = " (while video is not recorded)"
-            else:
-                extra = ""
             self.add_overlay(const.GRID_BOTTOMLEFT, "FPS", "%d%s" %
                             (1 / self._time_per_render, extra))
             self.add_overlay(const.GRID_BOTTOMLEFT, "Solver iterations", str(
@@ -403,79 +372,7 @@ class MjViewer(MjViewerBasic):
                 self._hide_overlay = not self._hide_overlay
             elif key == glfw.KEY_SPACE and self._paused is not None:  # stops simulation.
                 self._paused = not self._paused
-            # Advances simulation by one step.
-            elif key == glfw.KEY_RIGHT and self._paused is not None:
-                self._advance_by_one_step = True
-                self._paused = True
-            elif key == glfw.KEY_V or \
-                    (key == glfw.KEY_ESCAPE and self._record_video):  # Records video. Trigers with V or if in progress by ESC.
-                self._record_video = not self._record_video
-                if self._record_video:
-                    fps = (1 / self._time_per_render)
-                    self._video_process = Process(target=save_video,
-                                    args=(self._video_queue, self._video_path % self._video_idx, fps))
-                    self._video_process.start()
-                if not self._record_video:
-                    self._video_queue.put(None)
-                    self._video_process.join()
-                    self._video_idx += 1
-            elif key == glfw.KEY_T:  # capture screenshot
-                img = self._read_pixels_as_in_window()
-                imageio.imwrite(self._image_path % self._image_idx, img)
-                self._image_idx += 1
-            # elif key == glfw.KEY_I:  # drops in debugger.
-            #     try:
-            #         import ipdb
-            #         ipdb.set_trace()
-            #         print('You can access the simulator by self.sim')
-            #     except ImportError:
-            #         print('pip install ipdb to use debugger')
-            elif key == glfw.KEY_S:  # Slows down simulation.
-                self._run_speed /= 2.0
-            elif key == glfw.KEY_F:  # Speeds up simulation.
-                self._run_speed *= 2.0
-            elif key == glfw.KEY_C:  # Displays contact forces.
-                vopt = self.vopt
-                vopt.flags[10] = vopt.flags[11] = not vopt.flags[10]
-            elif key == glfw.KEY_D:  # turn off / turn on rendering every frame.
-                self._render_every_frame = not self._render_every_frame
 
-            elif key == glfw.KEY_E:
-                vopt = self.vopt
-                vopt.frame = 1 - vopt.frame
-
-            elif key == glfw.KEY_R:  # makes everything little bit transparent.
-                self._transparent = not self._transparent
-                if self._transparent:
-                    self.sim.model.geom_rgba[:, 3] /= 5.0
-                else:
-                    self.sim.model.geom_rgba[:, 3] *= 5.0
-            elif key == glfw.KEY_M:  # Shows / hides mocap bodies
-                self._show_mocap = not self._show_mocap
-                for body_idx1, val in enumerate(self.sim.model.body_mocapid):
-                    if val != -1:
-                        for geom_idx, body_idx2 in enumerate(self.sim.model.geom_bodyid):
-                            if body_idx1 == body_idx2:
-                                if not self._show_mocap:
-                                    # Store transparency for later to show it.
-                                    self.sim.extras[
-                                        geom_idx] = self.sim.model.geom_rgba[geom_idx, 3]
-                                    self.sim.model.geom_rgba[geom_idx, 3] = 0
-                                else:
-                                    self.sim.model.geom_rgba[
-                                        geom_idx, 3] = self.sim.extras[geom_idx]
-            elif key in (glfw.KEY_0, glfw.KEY_1, glfw.KEY_2, glfw.KEY_3, glfw.KEY_4):
-                self.vopt.geomgroup[key - glfw.KEY_0] ^= 1
-            elif glfw.get_key(window, glfw.KEY_LEFT_CONTROL) and 290 <= key <= 301:
-                # index into the last 12 elements of mjtVisFrame
-                keynum = min(key - 290 + 12, 21)
-                vopt = self.vopt
-                vopt.flags[keynum] = vopt.flags[keynum] = not vopt.flags[keynum]
-            elif 290 <= key <= 301:
-                # index into the first 12 elements of mjtVisFrame
-                keynum = key - 290
-                vopt = self.vopt
-                vopt.flags[keynum] = vopt.flags[keynum] = not vopt.flags[keynum]
             # adjust object location up / down
             # X
             elif key == glfw.KEY_O:
@@ -545,16 +442,3 @@ class MjViewer(MjViewerBasic):
                 self.planet = 'ISS'
 
             super().key_callback(window, key, scancode, action, mods)
-
-# Separate Process to save video. This way visualization is
-# less slowed down.
-
-
-def save_video(queue, filename, fps):
-    writer = imageio.get_writer(filename, fps=fps)
-    while True:
-        frame = queue.get()
-        if frame is None:
-            break
-        writer.append_data(frame)
-    writer.close()
